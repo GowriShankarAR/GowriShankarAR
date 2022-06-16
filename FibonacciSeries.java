dd1c22b7c0a536ead8a6241a43f4a90cdd32772c
GowriShankarAR <gowrishankar.ar.14@gmail.com>
2022-06-16 12:23:50 +0530

    Create FibonacciSeries.java

---
+++ FibonacciSeries.java
@@ -0,0 +1,26 @@
+package week1.Day1;
+
+public class FibonacciSeries {
+	
+	public static void main(String[] args) {
+		
+
+		int a=0;
+		int b=1;
+		int sum;
+		
+		System.out.println(a);
+		
+	for(int i=0;i<10;i++) {
+		
+		sum=a+b;
+		System.out.println(sum);
+		b=a;
+		a=sum;
+		
+	}
+		
+	}
+	
+}
+		
